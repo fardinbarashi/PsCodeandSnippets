26da11c31a6b3c09c809141065bc5331c21ec173
Code Ape <fardin.barashi@gmail.com>
2023-04-09 22:25:46 +0200

    Add files via upload

---
+++ Translate Sid.ps1
@@ -0,0 +1,13 @@
+﻿ $ComputerName = ""
+ 
+ $GetProfileSid = Get-WmiObject -ComputerName $ComputerName -Filter "Special=False" -Class Win32_UserProfile | Select-Object Sid
+            
+ foreach ($UserSid in $GetProfileAttributes) {     
+ 
+ $SID = New-Object System.Security.Principal.SecurityIdentifier($UserSid.sid)            
+ $UserAccount = $SID.Translate([System.Security.Principal.NTAccount])            
+ $Profile = $UserAccount.value.split("\")[1]  
+ $Username = $Profile
+ Write-Host $Username
+   
+    }
