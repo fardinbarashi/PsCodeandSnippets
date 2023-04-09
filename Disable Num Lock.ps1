cfb09481be68a40888a3261c3b1696f639c43ebe
Code Ape <fardin.barashi@gmail.com>
2023-04-09 22:27:25 +0200

    Add files via upload

---
+++ Disable Num Lock.ps1
@@ -0,0 +1,8 @@
+﻿<#
+Goes in to regEdit HKU\.DEFAULT\Control Panel\Keyboard and changes the value to 0
+#>
+
+# Run Query
+# Disable Num Lock
+Set-ItemProperty -Path 'Registry::HKU\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "0"
+
---
+++ Enable Num Lock.ps1
@@ -0,0 +1,14 @@
+﻿<#
+Goes in to regEdit HKU\.DEFAULT\Control Panel\Keyboard and changes the value to 1
+#>
+
+# Run Query
+
+# Enable Num Lock
+Set-ItemProperty -Path 'Registry::HKU\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "2"
+
+
+
+
+
+
